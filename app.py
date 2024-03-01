91d2f6cb691f8b537256d4926e966ac1c2b2d769
BharathiAmirtham <146439948+BharathiAmirtham@users.noreply.github.com>
2024-03-01 14:15:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template,request,redirect,url_for
+app = Flask(__name__)
+
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+
+@app.route('/hello')
+def helloworld():
+    return render_template('hello.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    err=None
+    if request.method =='POST':
+        if request.form['uname']!='Bharathi' or request.form['pwd']!='123':
+            err='invalid username and password!!'
+        else:
+            return redirect(url_for('home'))
+
+    return render_template('login.html',err=err)
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run()
